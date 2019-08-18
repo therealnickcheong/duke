59eddefe7c51ef5abc5d12e4212d876dd1957dd9
Nicholas Cheong <nicholas1cheong@gmail.com>
2019-08-18 11:08:43 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,6 +15,8 @@ public class Duke {
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
 
+        //String[] toDoList = {};
+        ArrayList<String> toDoList = new ArrayList<String>();
         boolean exitFlag = false;
         while(!exitFlag){
             Scanner myObj = new Scanner(System.in);  // Create a Scanner object
@@ -24,9 +27,17 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t____________________________________________________________");
                 exitFlag = true;
-            }else{
+            }else if(userCmd.equals("list")){
+                //list toDoList
                 System.out.println("\t____________________________________________________________");
-                System.out.println("\t" + userCmd);  // Output user inputS
+                for(int i = 0; i<toDoList.size(); i++){
+                    System.out.println("\t" + (i+1) + ". " + toDoList.get(i));
+                }
+                System.out.println("\t____________________________________________________________");
+            }else{
+                toDoList.add(userCmd);
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tadded: " + userCmd);  // Output user inputS
                 System.out.println("\t____________________________________________________________");
             }
 
