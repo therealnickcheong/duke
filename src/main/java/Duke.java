63fbb654a2f14e84b1ed7bbfe9682e1866e20673
Nicholas Cheong <nicholas1cheong@gmail.com>
2019-08-18 12:52:56 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,8 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
 
         //String[] toDoList = {};
-        ArrayList<String> toDoList = new ArrayList<String>();
+
+        ArrayList<Task> toDoList = new ArrayList<>();
         boolean exitFlag = false;
         while(!exitFlag){
             Scanner myObj = new Scanner(System.in);  // Create a Scanner object
@@ -27,15 +28,32 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t____________________________________________________________");
                 exitFlag = true;
+            }else if(userCmd.substring(0,4).equals("done")){
+                int taskNum = Integer.parseInt(userCmd.substring(userCmd.lastIndexOf(' ') + 1));  ;
+                //System.out.println(taskNum);
+                if(taskNum > toDoList.size()){
+                    System.out.println("index out of range");
+                }else{
+                    //System.out.println(toDoList.get(taskNum-1).description);
+                    toDoList.get(taskNum-1).markAsDone();
+                    System.out.println("\t____________________________________________________________");
+                    System.out.println("\tNice! I've marked this task as done:");
+                    System.out.println("\t" + "[" + toDoList.get(taskNum-1).getStatusIcon() + "] " + toDoList.get(taskNum-1).description);
+                    System.out.println("\t____________________________________________________________");
+                }
+
+
             }else if(userCmd.equals("list")){
                 //list toDoList
                 System.out.println("\t____________________________________________________________");
+                System.out.println("\tHere are the tasks in your list:");
                 for(int i = 0; i<toDoList.size(); i++){
-                    System.out.println("\t" + (i+1) + ". " + toDoList.get(i));
+                    System.out.println("\t" + (i+1) + ".[" + toDoList.get(i).getStatusIcon() + "] " + toDoList.get(i).description);
                 }
                 System.out.println("\t____________________________________________________________");
             }else{
-                toDoList.add(userCmd);
+                Task t = new Task(userCmd);
+                toDoList.add(t);
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\tadded: " + userCmd);  // Output user inputS
                 System.out.println("\t____________________________________________________________");
