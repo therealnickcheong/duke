e0ecb07f5c5534b23ef4c89d6ea3a1b7208773ef
Nicholas Cheong <nicholas1cheong@gmail.com>
2019-08-18 10:27:59 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+
+        //System.out.println("Hello from\n" + logo);
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+
+        boolean exitFlag = false;
+        while(!exitFlag){
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            String userCmd = myObj.nextLine();  // Read user input
+
+            if(userCmd.equals("bye")){
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t____________________________________________________________");
+                exitFlag = true;
+            }else{
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t" + userCmd);  // Output user inputS
+                System.out.println("\t____________________________________________________________");
+            }
+
+        }
+
     }
 }
